df897d0ce6029065ea72368c9177ef046feb2435
Euna-6 <eunah616@naver.com>
2022-02-27 00:19:41 +0900

    TEST : 뷰 동적 생성

--- app/src/main/java/com/example/youtubecleaner/ResultActivity.java
+++ app/src/main/java/com/example/youtubecleaner/ResultActivity.java
@@ -1,7 +1,11 @@
 package com.example.youtubecleaner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity{
 
-    //LinearLayout llName;
+    Button btnBlock1;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +26,29 @@ public class ResultActivity extends AppCompatActivity{
         actionBar.setDisplayHomeAsUpEnabled(true);  // 눌렀을때 발생하는 건 manifest에서
         actionBar.setTitle("");
 
+
+        //실험용. 버튼 누르면 Sub 클래스가 실행되면서 뷰가 동적 생성됨
+        btnBlock1 = (Button) findViewById(R.id.btnBlock1);
+        btnBlock1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Sub nLayout = new Sub(getApplicationContext());
+                LinearLayout con = (LinearLayout) findViewById(R.id.con);
+                con.addView(nLayout);
+            }
+        });
+
+    }
+}
+
+class Sub extends LinearLayout{
+    public Sub(Context context){
+        super(context);
+        init(context);
     }
 
-    /*
-    private void createTextView(){
-        TextView newTv = new TextView(getApplicationContext());
-        newTv.setText("원하는 텍스트");
-        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-        param.bottomMargin=8;
-        newTv.setLayoutParams(param);
-        llName.addView(newTv);
+    private void init(Context context){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.pra, this, true);
     }
-
-     */
 }
