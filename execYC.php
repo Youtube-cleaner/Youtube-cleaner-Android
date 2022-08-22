2affe17c7b383cfd09c86dc6aff506f5cf320375
Euna <79786441+Euna-6@users.noreply.github.com>
2022-08-22 23:07:24 +0900

    Update execYC.php

--- execYC.php
+++ execYC.php
@@ -7,20 +7,17 @@
 		die("DB | Connect Error : " . mysqli_connect_error());
 	}
 
-    echo "DB | Connected";
-
 	$youtubeID = $_GET["youtubeID"];
-    echo "GET | youtubeID = $youtubeID";
 	
 	exec("python hello.py ".$youtubeID);
-    echo "Python | exec";
 
 	// 변수는 선언만 하면 오류 발생.
 	$userID = "userID";
 	$comment = "comment";
 	$score = 0.0;
 
-	$statement = mysqli_prepare($conn, "SELECT userID, comment, score FROM YTtable WHERE youtubeID = ?");
+	//$statement = mysqli_prepare($conn, "SELECT userID, comment, score FROM YTtable WHERE youtubeID = ?");
+	$statement = mysqli_prepare($conn, "SELECT userID, comment, score FROM ?");
 	mysqli_stmt_bind_param($statement, "s", $youtubeID);
 	mysqli_stmt_execute($statement);
 	mysqli_stmt_bind_result($statement, $userID, $comment, $score);
@@ -28,7 +25,6 @@
 	//$query = "SELECT userID, comment, score FROM YTtable WHERE youtubeID=" .$youtubeID;
     //$query = "SELECT userID, comment, score FROM YTtable WHERE youtubeID='".$youtubeID."'";
 	//$result = mysqli_query($conn, $query);
-	echo "DB | query exec";
 	
 	$data = array();
 
@@ -36,21 +32,10 @@
 		$data["userID"] = $userID;
 		$data["comment"] = $comment;
 		$data["score"] = $score;
+		$result[] = $data;
 	}
 
-	/*
-	if(mysqli_num_rows($result)>0){
-		while ($row = mysqli_fetch_array($result)) {
-			array_push($data, array('userID'=>$row['userID'], 'comment'=>$row['comment'], 'score'=>$row['score']));
-		}
-		$json = json_encode(array("YTtable"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
-		echo $json;
-	} else {
-		echo "DB | SQL query Error : " . mysqli_error($conn);
-	}
-	*/
-
-	echo json_encode($data);
-
+	echo json_encode($result);
+	mysqli_stmt_close($statement);
 	mysqli_close($conn);
 ?>
